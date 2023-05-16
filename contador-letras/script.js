bdd6bef2813bf2478ba9ae0a7e88b3bb1ea4b790
IasminFernandes <126726115+IasminCQFernandes@users.noreply.github.com>
2023-05-16 20:57:31 -0300

    mini projetos de estudos

---
+++ contador-letras/script.js
@@ -0,0 +1,6 @@
+
+function calcular(){
+    var input = document.getElementById('palavra').value;
+   
+    document.getElementById('total').innerHTML = 'Total de Caracteres: '+ input.length;
+}
---
+++ cores/script.js
@@ -0,0 +1,23 @@
+alert("ESCOLHA SUA COR");
+
+function colorRed(){
+    document.querySelector('body').style = "background-color:red;"
+}
+
+function colorBlue(){
+    document.querySelector('body').style = "background-color:blue;"
+}
+
+function colorYellow(){
+    document.querySelector('body').style = "background-color:yellow;"
+}
+
+function colorPink(){
+    document.querySelector('body').style = "background-color:pink;"
+}
+
+function colorBlack(){
+    document.querySelector('body').style = "background-color:black;"
+}
+
+
